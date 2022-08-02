c35e1de7611c4469550a42a652386fd326083f82
jkstone321 <jkstone321@gmail.com>
2022-08-01 21:10:59 -0400

    started projects page and focused on mobile friendly

---
+++ portfolio2/src/pages/projects/index.js
@@ -0,0 +1,18 @@
+import "./style.css";
+
+function Projects() {
+  return (
+    <>
+      <div id="box">
+        <h1 id="title-name">Projects</h1>
+        <ul id="link-list">
+          <li>
+            <a href="/">Home</a>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default Projects;
